7f99953a40e4902b458785b71290c496a2e224e6
Keith Kamau <keith.kamau2@student.moringaschool.com>
2026-04-16 18:07:32 +0300

    Advanced Asynchronous JS Lab for random Social Media

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your code here!
+async function fetchAndDisplayPosts() {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const posts = await response.json();
+    displayPosts(posts);
+  }
+  catch (error) {
+    console.error("Error fetching posts:", error);
+  }
+}
+
+function displayPosts(posts) {
+  const postList = document.getElementById('post-list');
+
+  for (let i = 0; i < posts.length; i++) {
+    const post = posts[i];
+
+    const postContent = document.createElement("li");
+
+    const postHeading = document.createElement("h1");
+    postHeading.textContent = posts[i].title;
+
+    const postInfo = document.createElement("p");
+    postInfo.textContent = posts[i].body;
+
+    postContent.appendChild(postHeading);
+    postContent.appendChild(postInfo);
+
+    postList.appendChild(postContent);
+  }
+}
+
+fetchAndDisplayPosts();
